860f5d382a687fa2a83fbf9d9f67c105c96d0952
Immo Landwerth <immol@microsoft.com>
2017-10-17 13:55:44 -0700

    Remove hack for calling Process through reflection

--- src/PlatformCompat.Analyzers/Fixes/ReportIssueCodeFixProvider.cs
+++ src/PlatformCompat.Analyzers/Fixes/ReportIssueCodeFixProvider.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Composition;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
-using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
@@ -79,19 +78,7 @@ namespace PlatformCompat.Analyzers.Fixes
 
             public override void Apply(Workspace workspace, CancellationToken cancellationToken)
             {
-                // NOTE: This is a hack. Unfortunately, retargeting to .NET Standard 1.6/2.0 isn't straight forward
-                //       because this code has to run in VS and we don't have a good way to provide the facades and
-                //       binding redirects there.
-
-                var processClass = Type.GetType("System.Diagnostics.Process, System, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089");
-                var processClassTypeInfo = processClass.GetTypeInfo();
-                var startMethod = processClassTypeInfo
-                                    .GetDeclaredMethods("Start")
-                                    .Single(m => m.IsStatic &&
-                                                m.GetParameters().Length == 1 &&
-                                                m.GetParameters()[0].ParameterType == typeof(string));
-
-                startMethod.Invoke(null, new[] { FileName });
+                Process.Start(FileName);
             }
         }
     }
